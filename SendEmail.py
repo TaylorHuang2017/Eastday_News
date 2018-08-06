d10f8de98cf2afe236f14bdaca3fe1c89ccd33ad
Taylor Huang <tahuan@microsoft.com>
2018-08-06 09:29:52 +0800

    Bug fix
    Fix a bug in the logic, and allow the error message to be sent to the subscribed user for later review

--- SendEmail.py
+++ SendEmail.py
@@ -1,28 +1,45 @@
+import pandas as pd
 import datetime
 import requests
 from lxml import etree
+import os.path
+import time
+import logging
+import sys
 
-# 调用Mailgun发送HTML格式右键的函数
 # You can see a record of this email in your logs: https://app.mailgun.com/app/logs
 # You can send up to 300 emails/day from this sandbox server.
 # Next, you should add your own domain so you can send 10,000 emails/month for free.
-# 请替换你在Mailgun注册时获取的domain name和API key，以及注册的邮件地址
+
+
 def send_complex_message(subject, text):
     return requests.post(
-        "https://api.mailgun.net/v3/<Your Domain>/messages",
-        auth=("api", "<Your API key>"),
-        data={"from": "Mailgun Sandbox <postmaster@Your Domain>",
-              "to": "<Your registered recipient>",
-              "cc": "<Your registered recipient>",
+        "https://api.mailgun.net/v3/sandboxbc5425db5f0e46be99aa28b2746769d1.mailgun.org/messages",
+        auth=("api", "your api key"),
+        data={"from": "Mailgun Sandbox <postmaster@sandboxbc5425db5f0e46be99aa28b2746769d1.mailgun.org>",
+              "to": "your mailbox address",
+              "cc": "your mailbox address",
               "subject": subject,
               "text": "Testing some Mailgun awesomness!",
               "html": text})
 
-# 东方网新闻链接
-url = "http://www.eastday.com/eastday/shouye/node670813/n847507/20180704/index_T1722.html"
+mydate = datetime.datetime.now().strftime("%Y%m%d")
+url = "http://www.eastday.com/eastday/shouye/node670813/n847507/{0}/index_T1722.html".format(mydate)
 r = requests.get(url)
 text_to_send = ''
 
+# logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+log_path = os.path.dirname(os.getcwd()) + '/GetThemAll/Logs/'
+log_name = log_path + rq + '.log'
+logfile = log_name
+fh = logging.FileHandler(logfile, mode='w')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 try:
     r.raise_for_status()
@@ -34,12 +51,11 @@ try:
     list_of_time = s.xpath('//p[@class="gray14"]/text()')
     list_of_body = s.xpath('//div[@class="cnt-inner"]/div/a/text()')
 
-
     total_num = len(list_of_time)
 
     for j in range(total_num):
-        news_title = '//*[@id="maincnt"]/div[{0}]/div[3]/h2/a/text()'.format(j+1)   # 新闻标题
-        news_link = '//*[@id="maincnt"]/div[{0}]/div[3]/h2/a/@href'.format(j+1)     # 新闻详情的链接。注：某些新闻没有详情链接，所以要进行判断
+        news_title = '//*[@id="maincnt"]/div[{0}]/div[3]/h2/a/text()'.format(j+1)
+        news_link = '//*[@id="maincnt"]/div[{0}]/div[3]/h2/a/@href'.format(j+1)
         if ''.join(s.xpath(news_link)) != '':
             news_to_add = ''.join(s.xpath(news_title)) + ' ' + '<a href="' + ''.join(s.xpath(news_link)) + '">详细</a>'
         else:
@@ -49,31 +65,32 @@ try:
         list_of_images.append(''.join(s.xpath(image)))
 
 
-    # 天气预报
+    # weather
     text_to_send += '<html><head><title>直播上海</title></head><body>'
     text_to_send += ''.join(s.xpath('//*[@id="timebar"]/p[1]/text()'))
     text_to_send += '<br><br>'
 
 
     for i in range(total_num):
-        text_to_send += ''.join(list_of_time[i])  # 新闻发布时间
+        text_to_send += ''.join(list_of_time[i])
         text_to_send += '<br>'
-        text_to_send += ' ' + '<strong>' + list_of_title[i] + '</strong>' # 新闻标题及链接        
+        text_to_send += ' ' + '<strong>' + list_of_title[i] + '</strong>'
+        #text_to_send += ''.join(s.xpath('//*[@id="maincnt"]/div[{0}]/div[3]/h2/a/@href'.format(i+1)))
         text_to_send += '<br><br>'
         if list_of_images[i] != '':
-            text_to_send += '<img src="' + list_of_images[i] + '" resize height="123" width="200">'  # 新闻配图。注：某些新闻没有详情配图，所以要进行判断
+            text_to_send += '<img src="' + list_of_images[i] + '" resize height="123" width="200">'
             text_to_send += '<br><br>'
-        text_to_send += ''.join(list_of_body[i])   #新闻简介
+        text_to_send += ''.join(list_of_body[i])
         text_to_send += '<br><br>'
 
     text_to_send += '</body></html>'
 
+    subject = '直播上海 ' + ''.join(list_of_time[-1]) + ' --' + ''.join(list_of_time[0])[6:]
+    # send_simple_message(subject,text_to_send)
+    send_complex_message(subject, text_to_send)
+
 except Exception as exc:
-    print('There was a problem: %s' % (exc))
+    send_complex_message('东方网错误','unable to connect. {}'.format(exc))
 
-
-subject = '直播上海 ' + ''.join(list_of_time[-1]) + ' --' + ''.join(list_of_time[0])[6:]  # 标题格式为：直播上海 年-月-日 起始时间 -- 结束时间 
-
-send_complex_message(subject, text_to_send)  # 发邮件，通过计划任务，可实现每隔一定时间自动发邮件
-
+sys.exit()
 
